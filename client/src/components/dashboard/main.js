dd017b76ab816989eab0e2df1592e3b3654544ca
voon220 <peter220@gmail.com>
2023-01-29 15:57:58 +0800

    4th commits

--- client/src/components/dashboard/main.js
+++ client/src/components/dashboard/main.js
@@ -1,13 +1,26 @@
 import { AdminTitle } from '../../utils/tools'
-
+import { useSelector} from 'react-redux'; 
+import AnnouncementIcon from '@mui/icons-material/Announcement';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
 const DashboardMain = () => {
-
+    const users = useSelector(state => state.users);
 
     return(
         <div> 
             <AdminTitle title="Dashboard" />
-            **For owner who would like to upload their property listing to us, kindly verify your account then you will 
-            see a 'owner upload listing' button available.
+
+             { users.data.role === 'admin' ? <p><InsertEmoticonIcon className='smile-face'/>Welcome Back! we been missing you!</p>
+                : users.data.verified ? 
+                <p className='verify-reminder'><CheckCircleIcon className='smile-face'/> Your account is verified, please select ''Owner upload listing' to upload your property for us, we'll help you rent out as soon as possible!</p>
+                :
+                <p className='verify-reminder'> 
+                <AnnouncementIcon className='announce-icon'/>
+                Welcome to Rentbase! You have not verified your account yet, please check your email, once verified, you will be able to upload their property in Dashboard.
+                </p>
+            }
+
+       
         </div>
 
     )
